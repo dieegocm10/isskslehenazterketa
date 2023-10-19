8757da1b482e4ebcd8050bd60bda1004e8991e65
dieegocm10 <102172900+dieegocm10@users.noreply.github.com>
2023-10-20 01:55:53 +0200

    Update laburpenahash.py

--- laburpenahash.py
+++ laburpenahash.py
@@ -1,24 +1,24 @@
 import hashlib
 import os
-from PIL import Image
 
 def main():
-	aurkitua = False
-	
-	for argazkia in os.listdir("/home/diego/Descargas/irudia"):
-		ruta = os.path.join("/home/diego/Descargas/irudia", argazkia)
-		ruta = ("/home/diego/Descargas/irudia", argazkia)
-		if argazkia.endswith('.jpg'):
-			with open(ruta, 'rb') as izena:
-				edukia = izena.read()
-			hash_obj = hashlib.md5()
-			hash_obj.update(edukia)
-			hash_emaitza = hash_obj.hexdigest()
-			if hash_emaitza == "e5ed313192776744b9b93b1320b5e268" : 							
-				aurkitua = True
-				print("Zure irudia aurkitu da: ", argazkia)
-				
+    aurkitua = False
+    carpeta_irudia = "/home/diego/Descargas/irudia"
 
+    for argazkia in os.listdir(carpeta_irudia):
+        ruta = os.path.join(carpeta_irudia, argazkia)
+        if argazkia.endswith('.jpg'):
+            with open(ruta, 'rb') as izena:
+                edukia = izena.read()
+            hash_obj = hashlib.md5()
+            hash_obj.update(edukia)
+            hash_emaitza = hash_obj.hexdigest()
+            if hash_emaitza == "e5ed313192776744b9b93b1320b5e268":
+                aurkitua = True
+                print("Zure irudia aurkitu da:", argazkia)
 
-if __name__ == "__main__": 
-	main()
+    if not aurkitua:
+        print("Ez da zure irudirik aurkitu.")
+
+if __name__ == "__main":
+    main()
