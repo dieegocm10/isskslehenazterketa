61ab8237d8ef762d8a9b532ee5783bf3d1964636
dieegocm10 <102172900+dieegocm10@users.noreply.github.com>
2023-10-20 02:26:18 +0200

    Update laburpenahash.py

--- laburpenahash.py
+++ laburpenahash.py
@@ -3,25 +3,24 @@ import os
 from PIL import Image
 
 def main():
-	kont=1
-	aurkituta=False
-	while aurkituta==False:
-		irudiaPath= str(os.getcwd()) + "/irudia/imagen" + str(kont) + ".jpg"
-		print(f'{kont}. irudia:')
-		print("Gordetako lekua: " + str(irudiaPath))
-		with open(irudiaPath, 'rb') as artxiboa:
-                	edukia = artxiboa.read()
-		hash_obj = hashlib.md5()
-		hash_obj.update(edukia)
-		result=hash_obj.hexdigest()
-		print("Laburpena: " + str(result))
-		if result == "e5ed313192776744b9b93b1320b5e268":
-			aurkituta=True
-			print("Irudia hau irudi egokia izan da")
-		else:
-			
-			kont += 1
-			print("Irudia hau ez da irudi egokia")
-            
-if __name__ == "__main__":
-	main()
+    kont = 1
+    aurkituta = False
+    while not aurkituta:
+        irudiaPath = os.path.join(os.getcwd(), "irudia", "imagen" + str(kont) + ".jpg")
+        print(f'{kont}. irudia:')
+        print("Gordetako lekua: " + str(irudiaPath))
+        with open(irudiaPath, 'rb') as artxiboa:
+            edukia = artxiboa.read()
+        hash_obj = hashlib.md5()
+        hash_obj.update(edukia)
+        result = hash_obj.hexdigest()
+        print("Laburpena: " + str(result))
+        if result == "e5ed313192776744b9b93b1320b5e268":
+            aurkituta = True
+            print("Irudia hau irudi egokia izan da")
+        else:
+            kont += 1
+            print("Irudia hau ez da irudi egokia")
+
+if __name__ == "__main":
+    main()
